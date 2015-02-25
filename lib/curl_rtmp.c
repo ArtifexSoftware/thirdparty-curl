710915e3429efc8f3f017e76b55922846dfa20e7
Robin Watts <robin.watts@artifex.com>
2015-02-25 18:22:08 +0000

    Update Curl code and MSVC project for WIN64 build.

--- lib/curl_rtmp.c
+++ lib/curl_rtmp.c
@@ -39,7 +39,7 @@
 /* The last #include file should be: */
 #include "memdebug.h"
 
-#ifdef _WIN32
+#if defined(_WIN32) || defined(_WIN64)
 #define setsockopt(a,b,c,d,e) (setsockopt)(a,b,c,(const char *)d,(int)e)
 #define SET_RCVTIMEO(tv,s)   int tv = s*1000
 #else
